36d1db4ffcbaa74c844a1364ad69e41cd9f24531
Britney.Reginald <169092184+BritneyReginald@users.noreply.github.com>
2026-07-03 16:03:37 +0200

    Update index.ts

--- Sherqonline-backend/src/index.ts
+++ Sherqonline-backend/src/index.ts
@@ -11,7 +11,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://ohs-online-web.azurewebsites.net",
+  ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
